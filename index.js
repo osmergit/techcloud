4fb9171a6078ce685bb4edfd17a3b1cd35dd4df4
eduardruizgit <osmer.ruiz@esant.com.co>
2024-09-19 19:26:03 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const Animal = mongoose.model('Animal', new mongoose.Schema({
+  tipo: String,
+  estado: String,
+}))
+
+const app = express()
+
+mongoose.connect('mongodb+srv://clasesosmer:Colombia2024**@cluster0.touxv.mongodb.net/datapoli?retryWrites=true&w=majority&appName=Cluster0')
+
+app.get('/', async (_req, res) => {
+  console.log('listando... chanchitos...')
+  const animales = await Animal.find();
+  return res.send(animales)
+})
+app.get('/crear', async (_req, res) => {
+  console.log('creando...')
+  await Animal.create({ tipo: 'Perrito', estado: 'Feliz' })
+  return res.send('ok')
+})
+
+app.listen(3000, () => console.log('listening...'))
